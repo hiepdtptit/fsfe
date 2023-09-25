0fd19505ed1b703f9790d9fd20cf2f630bf035f8
hiepdt <hiepdt.ptit@gmail.com>
2023-09-25 23:58:28 +0700

    ADD DATABASE

---
+++ index-ws.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: __dirname});
+
+});
+
+server.on('request', app);
+server.listen(3000, function() { console.log('server started on port 3000');});
+
+
+
+process.on('SIGINT', () => {
+  wss.clients.forEach(function each(client) {
+    client.close()
+  })
+  server.close(() => {
+    shutdownDB();
+  })
+})
+/*** Begin websocket ***/
+
+const webSocketServer = require('ws').Server;
+
+const wss = new webSocketServer({ server: server })
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log('Clients connected', numClients);
+  console.log(ws, 'hello');
+  wss.broadcast(`Current visitors ${ numClients }`);
+
+
+  if(ws.readyState === ws.OPEN) {
+    ws.send('Welcome to my server')
+  }
+
+  db.run(`INSERT INTO visitors ( count, time ) 
+  
+    VALUES (${numClients}, datetime('now'))
+  `)
+
+  ws.on('close', function close() {
+    console.log('A client has disconnected!!!!')
+  })
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function forEach(client) {
+    client.send(data)
+  })
+}
+/** end websokets */
+/** start database */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database('./fsfe.db');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+})
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row)
+  })
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down db")
+  db.close()
+}
+
+
+
